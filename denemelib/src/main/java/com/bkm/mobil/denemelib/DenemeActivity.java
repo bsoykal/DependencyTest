d2a43a54e506958c988c878b262d9e282bececb7
burak <soykalburak@yahoo.com>
2016-11-28 10:36:07 +0300

    deneme helper

---
+++ denemelib/src/main/java/com/bkm/mobil/denemelib/DenemeActivity.java
@@ -0,0 +1,15 @@
+package com.bkm.mobil.denemelib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DenemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
---
+++ denemelib/src/main/java/com/bkm/mobil/denemelib/DenemeHelper.java
@@ -0,0 +1,14 @@
+package com.bkm.mobil.denemelib;
+
+import android.util.Log;
+
+/**
+ * Created by bsoykal on 28/11/2016.
+ */
+
+public class DenemeHelper {
+
+    public static void doThat(){
+        Log.i("deneme helper","it works");
+    }
+}
